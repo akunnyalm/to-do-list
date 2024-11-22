c3e9e00ba5e9e85131d35026db3702a6c21f72f1
L <100526085+akunnyalm@users.noreply.github.com>
2024-11-22 11:29:21 +0700

    firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDS_PyAfh-e-n98mtyZDVnOmWVaAJxXmuQ",
-    authDomain: "to-do-list-a1331.firebaseapp.com",
-    projectId: "to-do-list-a1331",
-    storageBucket: "to-do-list-a1331.firebasestorage.app", 
-    messagingSenderId: "867038842763",
-    appId: "1:867038842763:web:b9434fad6da4e2311ee98e",
-    measurementId: "G-54CQ52RVM3"
-};
+// paste your firebase config here
 
 // intialize Firebase & Firestore
 const app = initializeApp(firebaseConfig);
